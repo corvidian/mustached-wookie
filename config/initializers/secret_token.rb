78a3bc4a5531f4330d143633fc6063bead9db980
Topias Heinonen <corvus@iki.fi>
2014-05-09 19:13:49 +0300

    Heroku #3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MustachedWookie::Application.config.secret_key_base = '12da2624f87023138755db10db2ecad8c11276bc3faf987935039bd33b387f4326adb61b21f8ad53cc77faf474f105017f514ec85ff960f7153a711677884ad7'
